ce5820a752897d456f62ae6a4f25a751a296a03c
RonniePlay <ronnieplayyt@gmail.com>
2024-08-22 23:35:27 +0300

    Update execution

--- client/app/js/execution.js
+++ client/app/js/execution.js
@@ -2,8 +2,121 @@ import axios from "./components/axiosConfig.js";
 import { getColorByProcent } from "./components/colorConvert.js";
 import Execution from "./components/execution.js";
 
+async function openPair() {
+	const searchParams = new URLSearchParams(window.location.search);
+
+	const from = searchParams.get("exchangeFrom");
+	const to = searchParams.get("exchangeTo");
+	const buy = searchParams.get("buyPrice");
+	const sell = searchParams.get("sellPrice");
+	const spread = searchParams.get("spread");
+
+	const execution = document.querySelector(".execution");
+	const executionSubmit = document.querySelector(".execution .form-submit");
+	const executionInput = document.querySelector(".execution-form-input");
+	const input = document.querySelector(".execution-form-input input");
+
+	const executionProfit = document.querySelector(".execution-form__profit");
+	const executionPair = document.querySelector(".execution-form__pair");
+	const executionBuy = document.querySelector(".execution-form__buy");
+	const executionSell = document.querySelector(".execution-form__sell");
+	const executionNames = document.querySelectorAll(".exchange-field__name");
+
+	executionPair.textContent = `${from.toLocaleUpperCase()}_${to.toLocaleUpperCase()}`;
+	executionProfit.textContent = `${spread}%`;
+	executionBuy.textContent = `$${buy}`;
+	executionSell.textContent = `$${sell}`;
+	executionNames.item(0).textContent = from.toLocaleUpperCase();
+	executionNames.item(1).textContent = to.toLocaleUpperCase();
+
+	const convertProcent = (value) => Math.floor(value * 10) / 10;
+
+	executionSubmit.addEventListener("click", async () => {
+		executionSubmit.disabled = true;
+		executionInput.style.display = "none";
+
+		await axios
+			.post("/routes/create", {
+				exchangeFrom: executionNames.item(0).textContent.toLocaleLowerCase(),
+				exchangeTo: executionNames.item(1).textContent.toLocaleLowerCase(),
+				buyPrice: executionBuy.textContent.replace("$", "").trim(),
+				sellPrice: executionSell.textContent.replace("$", "").trim(),
+				spread: parseFloat(
+					executionProfit.textContent.replace(/[+%]/g, "").trim()
+				).toString(),
+				quantity: parseInt(input.value),
+			})
+			.then((res) => {
+				if (res.status === 201) {
+					const listOpen = document.querySelector(
+						".open-rout-table .table-body"
+					);
+					const emptyOpen = document.querySelector(".open-rout-table-empty");
+
+					if (emptyOpen) emptyOpen.remove();
+
+					const data = res.data;
+
+					const htmlString = `<div class="table-item">
+						<span>${data.exchangeFrom.toLocaleUpperCase()}-${data.exchangeTo.toLocaleUpperCase()}</span>
+						<span>${data.quantity}$</span>
+						<span>${convertProcent(data.spread)} %</span>
+						<span>${data.created}</span>
+						<span>${data.status}</span>
+						</div>`;
+					listOpen.insertAdjacentHTML("beforeend", htmlString);
+
+					// Время в секундах (2 часа, 48 минут и 16 секунд)
+					let totalTime = 2 * 3600 + 48 * 60 + 16;
+
+					// Функция для форматирования времени в ЧЧ:ММ:СС
+					function formatTime(seconds) {
+						let h = Math.floor(seconds / 3600);
+						let m = Math.floor((seconds % 3600) / 60);
+						let s = seconds % 60;
+						return `${h.toString().padStart(2, "0")}:${m
+							.toString()
+							.padStart(2, "0")}:${s.toString().padStart(2, "0")}`;
+					}
+
+					// Установить начальное значение таймера
+					executionSubmit.textContent = formatTime(totalTime);
+
+					// Запуск таймера с интервалом в 1 секунду
+					let timer = setInterval(() => {
+						totalTime--;
+						executionSubmit.textContent = formatTime(totalTime);
+
+						// Остановка таймера, когда время истекло
+						if (totalTime <= 0) {
+							clearInterval(timer);
+							executionSubmit.textContent = "Время истекло";
+						}
+					}, 1000);
+				}
+			});
+	});
+
+	execution.classList.add("open");
+}
+
 if (window.location.pathname === "/execution.html")
 	document.addEventListener("DOMContentLoaded", async () => {
+		const execution = document.querySelector(".execution");
+
+		const searchParams = new URLSearchParams(window.location.search);
+
+		const from = searchParams.get("exchangeFrom");
+		const to = searchParams.get("exchangeTo");
+		const buy = searchParams.get("buyPrice");
+		const sell = searchParams.get("sellPrice");
+		const spread = searchParams.get("spread");
+
+		if ((from, to, buy, sell, spread)) {
+			openPair();
+			execution.classList.add("open");
+		}
+
 		const listAll = document.querySelector("#table-1 .table-body");
 		const listTop = document.querySelector("#table-2 .table-body");
 		const listPremium = document.querySelector("#table-3 .table-body");
